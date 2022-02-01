854920a730cc8047c88609592a230c3b88eb5d34
EC2 Default User <ec2-user@ip-172-31-91-106.ec2.internal>
2022-02-01 06:12:42 +0000

    finished 17

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images
+    @post_images = PostImage.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update
+    redirect_to user_path(@user.id)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, profile_image)
+  end
+end
---
+++ db/migrate/20220131124720_create_post_images.rb
@@ -0,0 +1,13 @@
+class CreatePostImages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :post_images do |t|
+
+      t.string :shop_name
+      t.text :caption
+      t.integer :user_id
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
