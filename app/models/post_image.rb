c4d15f1f77ba18a49cebe8c409f68a7f4c9b0e25
EC2 Default User <ec2-user@ip-172-31-91-106.ec2.internal>
2022-05-04 08:15:46 +0000

    [start] practice rspec

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -11,11 +11,11 @@ class PostImage < ApplicationRecord
     favorites.exists?(user_id: user.id)
   end
 
-  def get_image
+  def get_image(size)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image
+    image.variant(resize: size).processed
   end
 end
